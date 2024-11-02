2bd0037e2b7f2992a708a3e09cae4a3eef06c2fd
Pillippa Perez Pons <ignacia.pp@gmail.com>
2024-11-02 15:29:10 -0700

    removed

--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,14 +2,14 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import { projectId, dataset } from './configuration'
 
 export default defineConfig({
   name: 'default',
   title: 'skyward',
 
-  projectId: 'ed4iyyjh',
-  dataset: 'production',
-
+  projectId,
+  dataset,
   plugins: [structureTool(), visionTool()],
 
   schema: {
