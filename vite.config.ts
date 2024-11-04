c09cf83a8860f5c4303fd22dc9f20105102c338a
Pillippa Perez Pons <ignacia.pp@gmail.com>
2024-11-04 07:35:47 -0800

    new config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from 'vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 
 declare module "@remix-run/node" {
@@ -8,17 +8,26 @@ declare module "@remix-run/node" {
   }
 }
 
-export default defineConfig({
-  plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
-    tsconfigPaths(),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      remix({
+        future: {
+          v3_fetcherPersist: true,
+          v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
+          v3_singleFetch: true,
+          v3_lazyRouteDiscovery: true,
+        },
+      }),
+      tsconfigPaths(),
+    ],
+    define: {
+        'process.env.SANITY_STUDIO_STEGA_ENABLED': env.SANITY_STUDIO_STEGA_ENABLED,
+        'process.env.SANITY_STUDIO_PROJECT_ID': env.SANITY_STUDIO_PROJECT_ID,
+        'process.env.SANITY_STUDIO_DATASET': env.SANITY_STUDIO_DATASET,
+    },
+  }
 });
